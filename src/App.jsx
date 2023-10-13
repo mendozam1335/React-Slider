5a6a646915d9f832b8ffdda1af8ff444c0de370f
mendozam1335 <55033050+mendozam1335@users.noreply.github.com>
2023-10-13 18:33:05 -0500

    Changed to using Slick-Slider library

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { shortList, list, longList } from "./data";
 import Carousel from "./Components/Carousel";
 import PrevButton from "./Components/PrevButton";
 import NextButton from "./Components/NextButton";
+import SlickCarousel from "./Components/SlickCarousel";
 
 const App = () => {
   const [list, setList] = useState(longList);
@@ -19,13 +20,23 @@ const App = () => {
       return result;
     });
   };
+  useEffect(() => {
+    const sliderID = setInterval(() => {
+      nextSlide();
+    }, 2000);
+
+    return () => {
+      clearInterval(sliderID);
+    };
+  }, [currentSlide]);
   return (
     <main>
-      <section className="slider-container">
+      {/* <section className="slider-container">
         <Carousel people={list} currentSlide={currentSlide} />
         <NextButton onButtonClick={nextSlide} />
         <PrevButton onButtonClick={prevSlide} />
-      </section>
+      </section> */}
+      <SlickCarousel />
     </main>
   );
 };
